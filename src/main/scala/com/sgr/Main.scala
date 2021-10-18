1c454e8a5bc24cef8d97000c72471d5f3ce86a7c
sunguorui <17861418176@163.com>
2021-10-18 21:21:38 +0800

    demand one

--- src/main/scala/com/sgr/Main.scala
+++ src/main/scala/com/sgr/Main.scala
@@ -1,11 +1,39 @@
 package com.sgr
 
-import com.sgr.util.ShowData
+import com.sgr.util.GetData
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, count, lit, max}
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val dataShow = new ShowData
+
+    // 需求 1：计算中签率与倍率之间的量化关系
+    val luckyDogsDF: DataFrame = GetData.luckyNumbersDf
+    val applyNumbersDF: DataFrame = GetData.applyNumbersDF
+
+    // 过滤2016年以后的中签数据，且仅抽取中签号码carNum字段
+    val filterLuckyDogs: DataFrame = luckyDogsDF.filter(col("batchNum") >= "201601").select("carNum")
+    // 摇号数据与中签数据做内关联，Join Key为中签号码carNum
+    val joinDf: DataFrame = applyNumbersDF.join(filterLuckyDogs,Seq("carNum"), "inner")
+    // 统计中签者的倍率 同一个号码在不同的批次中签率是不同的
+    // |  201706|0007100221303|         9|
+    //|  201705|0007100221303|         9|
+    //|  201704|0007100221303|         9|
+    val multipliers: DataFrame = joinDf.groupBy(col("batchNum"), col("carNum"))
+      .agg(count(lit(1)).alias("multiplier"))
+
+    // 只需要关注中签者在那个倍率下中的签即可  也就是中签号码的最大倍率
+    val uniqueMultipliers: DataFrame = multipliers.groupBy("carNum")
+      .agg(max("multiplier").alias("multiplier"))
+
+    val result: DataFrame = uniqueMultipliers.groupBy(col("multiplier"))
+      .agg(count(lit(1)).alias("cnt"))
+      .orderBy("multiplier")
+
+    result.show()
+
+
   }
 
 }
---
+++ src/main/scala/com/sgr/util/GetData.scala
@@ -0,0 +1,27 @@
+package com.sgr.util
+
+import org.apache.spark.sql.DataFrame
+
+object GetData extends LazyLogging{
+  // 表
+  // apply  2011-2019 年各个批次参与摇号的申请号码
+  // lucky 各个批次中签的申请号码
+
+  // 列
+  // carNum 申请号码
+  // batchNum 摇号批次
+
+  private val sparkSession = SparkSQLEnv.sparkSession
+
+  private val rootPath: String = "/car_data"
+
+  private val hdfs_path_apply: String = s"${rootPath}/apply"
+  val applyNumbersDF: DataFrame = sparkSession.read.parquet(hdfs_path_apply)
+//  logger.info("apply information")
+//  applyNumbersDF.show()
+
+  private val hdfs_path_lucky: String  = s"${rootPath}/lucky"
+  val luckyNumbersDf: DataFrame = sparkSession.read.parquet(hdfs_path_lucky)
+//  logger.info("lucky information")
+//  applyNumbersDF.show()
+}
